a2b59b428613e58d12b0400c9a71ae790a2cb5cb
yangga <yangga0070@gmail.com>
2018-04-16 02:19:32 +0900

    refac

---
+++ test/chart/history.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import 'mocha';
+import * as Lib from '../../src';
+
+describe('chart', () => {
+
+  describe('history', () => {
+
+    it('manual', () => {
+      const api = Lib.Chart.Exchanges.factoryApi('upbit');
+
+      api.history(new Lib.Chart.Exchanges.Interface.ChartHistoryParam({
+          base: 'BTC',
+          quote: 'KRW',
+          resolution: Lib.Chart.Exchanges.Interface.ChartHistoryResolutions.Minute,
+          resolutionValue: 30,
+      }), (err: any, result: any): void => {
+          // console.log('result', err, result);
+          expect(err).to.equal(null);
+      });
+    });
+
+    it('helper', () => {
+      const param = new Lib.Chart.Helpers.HistoryParam({
+        resolution: '30M',
+        symbol: 'UPBIT:BTC-KRW',
+      });
+
+      Lib.Chart.Helpers.history(param, (err: any, result: any): void => {
+        // console.log('result', err, result);
+        expect(err).to.equal(null);
+      });
+
+    });
+
+  });
+
+});
