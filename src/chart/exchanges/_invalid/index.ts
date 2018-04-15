96040c3fdf9690659c113c5426e0264a476ecd41
yangga <yangga0070@gmail.com>
2018-04-16 02:14:02 +0900

    refac

--- src/chart/exchanges/_invalid/index.ts
+++ src/chart/exchanges/_invalid/index.ts
@@ -3,7 +3,13 @@ import { ChartConfig, ChartHistoryParam, IChartUdfIf } from '../_interface';
 export class Api implements IChartUdfIf {
   public config(): ChartConfig {
     assert(0, "it shouldn't be called");
-    return new ChartConfig();
+    const cfg = new ChartConfig();
+    cfg.supports_search = false;
+    cfg.supports_group_request = false;
+    cfg.supported_resolutions = [];
+    cfg.supports_marks = false;
+    cfg.supports_time = false;
+    return cfg;
   }
 
   public history(
@@ -11,5 +17,6 @@ export class Api implements IChartUdfIf {
     callback: (error: any, result: any) => void,
   ): void {
     assert(0, "it shouldn't be called");
+    callback(new Error('it called invalid exchange\'s history'), null);
   }
 }
